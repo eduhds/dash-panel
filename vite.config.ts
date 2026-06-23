63308889c1cef64f52a0ecbb5b3e7c7ee2ed502b
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2026-06-23 14:21:12 -0300

    chore: setup tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 import babel from '@rolldown/plugin-babel';
+import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
