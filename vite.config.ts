08c5906af927e6ed0b96bc0ba87193913976e527
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2026-06-23 14:12:37 -0300

    chore: setup absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })]
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      '@': './src'
+    }
+  }
 });
